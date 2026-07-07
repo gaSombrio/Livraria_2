ad7bacd930544945187a96770a44656d3ad41f38
gaSombrio <sombrio.ga@gmail.com>
2026-07-07 14:04:35 -0300

    Primeiro commit

---
+++ core/serializers/compra.py
@@ -0,0 +1,36 @@
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
+
+from core.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    titulo = CharField(source='livro.titulo', read_only=True)
+    editora = CharField(source='livro.editora.nome', read_only=True)
+    preco = CharField(source='livro.preco', read_only=True)
+    capa = CharField(source='livro.capa.url', read_only=True)
+
+    total = SerializerMethodField()
+
+    def get_total(self, item):
+        return item.livro.preco * item.quantidade
+
+    class Meta:
+        model = ItensCompra
+        fields = (
+            'titulo',
+            'editora',
+            'quantidade',
+            'preco',
+            'total',
+            'capa'
+        )
+
+
+class CompraSerializer(ModelSerializer):
+    usuario = CharField(source='usuario.email', read_only=True)
+    status = CharField(source='get_status_display', read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Compra
+        fields = ('id', 'usuario', 'status', 'total', 'itens')
